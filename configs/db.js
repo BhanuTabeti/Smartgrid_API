6de85356c6cad56977bbe976a70ac7004eb04b73
Shashank Varanasi <es16btech11025@iith.ac.in>
2020-04-02 01:25:51 +0530

    add defaults to db.js

--- configs/db.js
+++ configs/db.js
@@ -2,10 +2,10 @@ const mysql = require("mysql");
 
 const pool = mysql.createPool({
   connectionLimit: 10,
-  host: process.env.HOST,
-  user: process.env.USERNAME,
-  password: process.env.PASSWORD,
-  database: process.env.DB
+  host: process.env.HOST || "default value",
+  user: process.env.USERNAME || "default value",
+  password: process.env.PASSWORD || "default value",
+  database: process.env.DB || "default value"
 });
 
 exports.pool = pool;
