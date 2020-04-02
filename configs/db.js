05f624cb335e63641ba7ac066b2de6b71d135c22
Bhanu Prakash Goud Tabeti <es16btech11022@iith.ac.in>
2020-04-02 23:54:18 +0530

    Add Dockerfile

--- configs/db.js
+++ configs/db.js
@@ -5,64 +5,64 @@ const mysql = require("mysql");
 var pool = {};
 
 pool.vvit_con = mysql.createPool({
-	host: process.env.HOST,
-	port: process.env.PORT,
-	user: process.env.USER,
-	password: process.env.PASSWORD,
+	host: process.env.DB_HOST,
+	port: process.env.DB_PORT,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
 	database: process.env.VVIT_DB,
 	socketPath: process.env.SOCKETPATH
 });
 
 pool.ouce_con = mysql.createPool({
-	host: process.env.HOST,
-	port: process.env.PORT,
-	user: process.env.USER,
-	password: process.env.PASSWORD,
+	host: process.env.DB_HOST,
+	port: process.env.DB_PORT,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
 	database: process.env.OUCE_DB,
 	socketPath: process.env.SOCKETPATH
 });
 
 
 pool.gnits_con = mysql.createPool({
-	host: process.env.HOST,
-	port: process.env.PORT,
-	user: process.env.USER,
-	password: process.env.PASSWORD,
+	host: process.env.DB_HOST,
+	port: process.env.DB_PORT,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
 	database: process.env.GNITS_DB,
 	socketPath: process.env.SOCKETPATH
 });
 
 
 pool.pvpsit_con = mysql.createPool({
-	host: process.env.HOST,
-	port: process.env.PORT,
-	user: process.env.USER,
-	password: process.env.PASSWORD,
+	host: process.env.DB_HOST,
+	port: process.env.DB_PORT,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
 	database: process.env.PVPSIT_DB,
 	socketPath: process.env.SOCKETPATH
 });
 
 
 pool.demo_con = mysql.createPool({
-	host: process.env.HOST,
-	port: process.env.PORT,
-	user: process.env.USER,
-	password: process.env.PASSWORD,
+	host: process.env.DB_HOST,
+	port: process.env.DB_PORT,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
 	database: process.env.DEMO_DB,
 	socketPath: process.env.SOCKETPATH
 });
 
 pool.demo_bvrith_con = mysql.createPool({
-	host: process.env.HOST,
-	port: process.env.PORT,
-	user: process.env.USER,
-	password: process.env.PASSWORD,
+	host: process.env.DB_HOST,
+	port: process.env.DB_PORT,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
 	database: process.env.BVRITH_DB,
 	socketPath: process.env.SOCKETPATH
 });
 
 pool.root_con = mysql.createPool({
-	host: process.env.HOST,
+	host: process.env.DB_HOST,
 	port: process.env.ROOT_PORT,
 	user: process.env.ROOT_USER,
 	password: process.env.ROOT_PASSWORD,
@@ -71,10 +71,10 @@ pool.root_con = mysql.createPool({
 });
 
 pool.bh_con = mysql.createPool({
-	host: process.env.HOST,
-	port: process.env.PORT,
-	user: process.env.USER,
-	password: process.env.PASSWORD,
+	host: process.env.DB_HOST,
+	port: process.env.DB_PORT,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
 	database: process.env.IITH_SS_DB
 });
 
